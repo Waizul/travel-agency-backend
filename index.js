4a2fb199e7c7aa2760502a2a8882c76b65716ae2
Waizul Haque <waiul@gmail.com>
2021-10-29 03:37:43 +0600

    data

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q5fov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -17,17 +18,14 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		await client.connect();
-		const database = client.db('insertDB');
-		const haiku = database.collection('haiku');
-		// create a document to insert
-		const doc = {
-			title: 'Record of a Shriveled Datum',
-			content: 'No bytes, no problem. Just insert a document, in MongoDB',
-		};
-		const result = await haiku.insertOne(doc);
-		console.log(
-			`A document was inserted with the _id: ${result.insertedId}`,
-		);
+		const database = client.db('traveldb');
+		const placeCollection = database.collection('places');
+
+		app.get('/places', async (req, res) => {
+			const cursor = placeCollection.find({});
+			const result = await cursor.toArray();
+			res.send(result);
+		});
 	} finally {
 		// await client.close()
 	}
